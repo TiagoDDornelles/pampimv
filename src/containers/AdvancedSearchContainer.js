4e74b78e753674ff37a5fc2bf7c32ede8b03497a
gabrielfogaca <gabriel.fogaca@unijui.edu.br>
2022-10-22 23:21:27 -0300

    tentando usar react

---
+++ src/containers/AdvancedSearchContainer.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getPropertyList } from '../redux/actions/propertiesAction';
+
+import { FormWrapper, Form } from '../components';
+
+import { priceFormat } from '../helpers/helper_functions';
+
+const AdvancedSearchContainer = () => {
+  return (
+    <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  );
+};
+
+export default AdvancedSearchContainer;
---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import links from '../constants/routes/nav-links';
+import { HeaderWrapper, Banner, Jumbotron } from '../components';
+import wpp from '../images/wpp.png';
+import logo from '../images/logo.png';
+
+import { AdvancedSearchContainer, SideNavigationContainer } from './index';
+
+const HeaderContainer = ({ bg, source }) => {
+  const [sideNavShown, setSideNavShown] = useState(false);
+  const [sideNavHidden, setSideNavHidden] = useState(true);
+  const [fixed, setFixed] = useState(false);
+
+  const changeBackgroundColorAndPosition = () => {
+    if (window.pageYOffset > 100) {
+      setFixed(true);
+    } else {
+      setFixed(false);
+    }
+  };
+
+  window.addEventListener('scroll', changeBackgroundColorAndPosition);
+
+  const handleSideNavigation = () => {
+    setSideNavHidden((prevState) => !prevState);
+    setSideNavShown((prevState) => !prevState);
+  };
+  return (
+    <Banner bg={bg} source={source}>
+      <HeaderWrapper bg={bg} fixed={fixed}>
+        <HeaderWrapper.Container>
+          <HeaderWrapper.Title bg={bg}>
+            <HeaderWrapper.Link bg={bg} fixed={fixed} to="/">
+              <img
+                src={wpp}
+                alt=""
+                style={{ position: 'relative', width: '30px', top: '7px' }}
+              />
+              (55) 9 9999 9999
+            </HeaderWrapper.Link>
+          </HeaderWrapper.Title>
+        </HeaderWrapper.Container>
+        <HeaderWrapper.Container>
+          <HeaderWrapper.Logotipo>
+            <img src={logo} alt="" />
+          </HeaderWrapper.Logotipo>
+        </HeaderWrapper.Container>
+      </HeaderWrapper>
+      {bg === 'true' && (
+        <Jumbotron>
+          <Jumbotron.Centro>
+            <Jumbotron.Title>Pampa Imoveis</Jumbotron.Title>
+            <Jumbotron.Text>Aqui você vive com conforto.</Jumbotron.Text>
+          </Jumbotron.Centro>
+          <Jumbotron.Right>
+            <AdvancedSearchContainer />
+          </Jumbotron.Right>
+        </Jumbotron>
+      )}
+      <SideNavigationContainer
+        sideNavShown={sideNavShown}
+        sideNavHidden={sideNavHidden}
+        setSideNavHidden={setSideNavHidden}
+        setSideNavShown={setSideNavShown}
+      />
+    </Banner>
+  );
+};
+
+export default HeaderContainer;
